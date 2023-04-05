d0070d84d26d9e8b3fc75c9cb815b03cbafa130e
Fantomet <Sigurd.Evensen@hotmail.com>
2023-04-05 21:19:23 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+import pygame 
+
+# -- Global Constants --
+width = 800
+height = 800
+background_colour = (255,255,255)
+
+running = True
+
+def main(running):
+    #draw the screen
+    draw()
+
+    while running:
+        ev = pygame.event.get()
+
+        for event in ev:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+                    break
+        
+        draw_circle()
+
+# -- Functions --
+def draw():
+    global screen
+
+    pygame.init()
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("My Game")
+    screen.fill(background_colour)
+    pygame.display.update()
+
+def draw_circle():
+    #draw a circle
+    pygame.draw.circle(screen, (0,0,0), (400,400), 50)
+    pygame.display.update()
+
+# -- Main Program --
+
+if __name__ == "__main__":
+    main(running)
