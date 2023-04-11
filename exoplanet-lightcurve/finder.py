14f7cc56596e59efe1bbd64035f503101a6b260c
Fantomet <Sigurd.Evensen@hotmail.com>
2023-04-11 19:39:49 +0200

    Update finder.py

--- exoplanet-lightcurve/finder.py
+++ exoplanet-lightcurve/finder.py
@@ -1,5 +1,7 @@
-from lightkurve import search_targetpixelfile
-from lightkurve import TessTargetPixelFile
-import lightkurve as lk
+import lightkurve as lk # type: ignore
 import numpy as np
+from lightkurve import TessTargetPixelFile, search_targetpixelfile # type: ignore
 
+pixelFile = search_targetpixelfile("KIC 6922244", author="Kepler", cadence="long", quarter=4).download()
+
+pixelFile.plot(frame=42)
